7ca477a5f6dfc9b6b73454778dc90541c4b684a1
Matheus de Barros <162741395+barrosmatheusc@users.noreply.github.com>
2024-07-03 15:13:46 -0300

    upd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, request, flash
+import json
+
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'MATHEUSB'
@@ -15,11 +17,22 @@ def login():
     nome = request.form.get('nome')    
     senha = request.form.get('senha')
    
-    if nome == "matheus" and senha == "1234":
-        return render_template("homeusuario.html")
-    else:
-        flash('USUÁRIO ou SENHA INVÁLIDOS')
-        return redirect('/')
+    with open('usuarios.json') as usuariosTemp:
+        usuario = json.load(usuariosTemp)
+        cont = 0
+        for usuario in usuario:
+            cont += 1
+            if usuario['nome'] == nome and usuario['senha'] == senha:
+                return render_template("homeusuario.html")
+            if cont >= len(usuario):
+                flash('USUÁRIO OU SENHA INVÁLIDO')
+                return redirect("/")
+
+    # if nome == "matheus" and senha == "1234":
+    #     return render_template("homeusuario.html")
+    # else:
+    #     flash('USUÁRIO ou SENHA INVÁLIDOS')
+    #     return redirect('/')
 
 
 if __name__ in "__main__":
